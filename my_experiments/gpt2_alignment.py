c5db69feeea7da8a584d86226b6857ffce608f52
Mara Pislar <pislarmara589@gmail.com>
2024-01-24 14:42:35 +0100

    add causal models

--- my_experiments/gpt2_alignment.py
+++ my_experiments/gpt2_alignment.py
@@ -5,6 +5,8 @@ from transformers import Trainer, TrainingArguments
 from transformers import TextDataset, DataCollatorForLanguageModeling
 from transformers import PreTrainedTokenizerFast, GPT2LMHeadModel, GPT2TokenizerFast, GPT2Tokenizer
 from sklearn.metrics import classification_report
+from pyvene import CausalModel
+import numpy as np
 
 def generate_sum_examples(num_examples=100):
     prompts = []
@@ -16,14 +18,20 @@ def generate_sum_examples(num_examples=100):
         num2 = random.randint(1, 10)
         num3 = random.randint(1, 10)
 
-        prompt = f"Calculate {num1} + {num2} + {num3} = "
-        answer = str(num1 + num2 + num3)
+        prompt = f"{num1}+{num2}+{num3}="
+        answer = num1 + num2 + num3
         full_text.append(f"{prompt}{answer}")
 
         prompts.append(prompt)
-        answers.append(answer)
+        answers.append(str(answer))
 
     return prompts, answers, full_text
+
+def input_sampler():
+    A = randNum()
+    B = randNum()
+    C = randNum()
+    return {"X":A, "Y":B, "Z":C}
 
 def generate_file(file_path, num_examples = 10000):
 
@@ -129,34 +137,130 @@ def eval_finetuned_gpt2(num_examples=100):
     else:
         print("Accuracy is 0.")
 
+def randvec(n=50, lower=-1, upper=1):
+    return np.array([round(random.uniform(lower, upper), 2) for i in range(n)])
+
+def randNum(lower=1, upper=10):
+        return random.randint(lower, upper)
+
+def causal_model_1():
+
+    variables =  ["X", "Y", "Z", "P", "O"]
+    number_of_entities = 20
+
+    reps = [randNum() for _ in range(number_of_entities)]
+    values = {variable:reps for variable in ["X", "Y", "Z"]}
+    values["P"] = list(range(2, 21))
+    values["O"] = list(range(3, 31))
+
+    parents = {"X":[], "Y":[], "Z":[], 
+            "P":["X", "Y"],
+            "O":["P", "Z"]}
+
+    def FILLER():
+        return reps[0]
+
+    functions = {"X":FILLER, "Y":FILLER, "Z":FILLER, 
+                "P": lambda x,y: x+y,
+                "O": lambda x,y: x+y}
+
+    pos = {"X":(1,0.1), "Y":(2,0.2), "Z":(2.8,0), 
+            "P":(1,2),
+            "O":(1.5,3)}
+
+    return CausalModel(variables, values, parents, functions, pos = pos)
+
+def causal_model_2():
+    
+    variables =  ["X", "Y", "Z", "P", "O"]
+    number_of_entities = 20
+
+    reps = [randNum() for _ in range(number_of_entities)]
+    values = {variable:reps for variable in ["X", "Y", "Z"]}
+    values["P"] = list(range(2, 21))
+    values["O"] = list(range(3, 31))
+
+    parents = {"X":[], "Y":[], "Z":[], 
+            "P":["Y", "Z"],
+            "O":["X", "P"]}
+
+    def FILLER():
+        return reps[0]
+
+    functions = {"X":FILLER, "Y":FILLER, "Z":FILLER, 
+                "P": lambda x,y: x+y,
+                "O": lambda x,y: x+y}
+
+    pos = {"X":(1,0.1), "Y":(2,0.2), "Z":(2.8,0), 
+            "P":(2,1),
+            "O":(1.5,3)}
+
+    return CausalModel(variables, values, parents, functions, pos = pos)
+
+def causal_model_3():
+    
+    variables =  ["X", "Y", "Z", "P", "O"]
+    number_of_entities = 20
+
+    reps = [randNum() for _ in range(number_of_entities)]
+    values = {variable:reps for variable in ["X", "Y", "Z"]}
+    values["P"] = list(range(2, 21))
+    values["O"] = list(range(3, 31))
+
+    parents = {"X":[], "Y":[], "Z":[], 
+            "P":["X", "Z"],
+            "O":["P", "Y"]}
+
+    def FILLER():
+        return reps[0]
+
+    functions = {"X":FILLER, "Y":FILLER, "Z":FILLER, 
+                "P": lambda x,y: x+y,
+                "O": lambda x,y: x+y}
+
+    pos = {"X":(1,0.1), "Y":(2,0.2), "Z":(2.8,0), 
+            "P":(1,2),
+            "O":(1.5,3)}
+
+    return CausalModel(variables, values, parents, functions, pos = pos)
+
 def main():
 
-    _, tokenizer, gpt2 = pyvene.create_gpt2_lm()
-    tokenizer.pad_token = tokenizer.eos_token
-    _ = gpt2.to("cuda")
+    # _, tokenizer, gpt2 = pyvene.create_gpt2_lm()
+    # tokenizer.pad_token = tokenizer.eos_token
+    # _ = gpt2.to("cuda")
 
-    train_file_path = "/gpfs/home1/mpislar/align-transformers/my_experiments/sum_training_data/training_sums.txt"
+    # train_file_path = "/gpfs/home1/mpislar/align-transformers/my_experiments/sum_training_data/training_sums.txt"
 
-    generate_file(train_file_path, 1280000)
+    # generate_file(train_file_path, 1280000)
 
-    output_dir = "/gpfs/home1/mpislar/align-transformers/result/"
-    overwrite_output_dir = False
-    batch_size = 64
-    num_train_epochs = 10
-    save_steps = 500
+    # output_dir = "/gpfs/home1/mpislar/align-transformers/result/"
+    # overwrite_output_dir = False
+    # batch_size = 64
+    # num_train_epochs = 30
+    # save_steps = 500
 
-    train(
-        train_file_path=train_file_path,
-        model=gpt2,
-        tokenizer=tokenizer,
-        output_dir=output_dir,
-        overwrite_output_dir=overwrite_output_dir,
-        batch_size=batch_size,
-        num_train_epochs=num_train_epochs,
-        save_steps=save_steps
-    )
+    # train(
+    #     train_file_path=train_file_path,
+    #     model=gpt2,
+    #     tokenizer=tokenizer,
+    #     output_dir=output_dir,
+    #     overwrite_output_dir=overwrite_output_dir,
+    #     batch_size=batch_size,
+    #     num_train_epochs=num_train_epochs,
+    #     save_steps=save_steps
+    # )
 
-    eval_finetuned_gpt2(100)
+    # eval_finetuned_gpt2(100)
+
+    n_examples = 1000
+
+    causal_model = causal_model_1()
+
+    X, y = causal_model.generate_factual_dataset(n_examples, input_sampler)
+    X = X.unsqueeze(1)
+    print(X[0])
+    print(y[0])
 
 if __name__ =="__main__":
     main()
