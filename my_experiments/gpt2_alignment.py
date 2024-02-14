3561cde6e634d82b3b93266dfb8b4d6227a8b238
Mara Pislar <pislarmara589@gmail.com>
2024-02-14 15:24:06 +0100

    fix bug

--- my_experiments/gpt2_alignment.py
+++ my_experiments/gpt2_alignment.py
@@ -294,7 +294,7 @@ def tokenizePrompt(prompt):
     tokenizer = load_tokenizer("/gpfs/home1/mpislar/align-transformers/result/")
     model = load_model("/gpfs/home1/mpislar/align-transformers/result/")
     # prompt = f"{tokenizer.decode(prompt['X'].item(), skip_special_tokens=True)}+{tokenizer.decode(prompt['Y'].item(), skip_special_tokens=True)}+{tokenizer.decode(prompt['Z'].item(), skip_special_tokens=True)}="
-    # prompt = f"{prompt['X']}+{prompt['Y']}+{prompt['Z']}=" # prompt for numerical causal model
+    prompt = f"{prompt['X']}+{prompt['Y']}+{prompt['Z']}=" # prompt for numerical causal model
     # return tokenizer.encode(prompt, return_tensors='pt')
     
     # version 4
