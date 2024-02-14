6efc40eec723dd73825b3a056ed52b6eb96487ec
Mara Pislar <pislarmara589@gmail.com>
2024-02-14 13:33:58 +0100

    changes

--- my_experiments/gpt2_alignment.py
+++ my_experiments/gpt2_alignment.py
@@ -294,7 +294,8 @@ def train_gpt2(causal_model, n_examples):
 
 def tokenizePrompt(prompt):
     tokenizer = load_tokenizer("/gpfs/home1/mpislar/align-transformers/result/")
-    prompt = f"{tokenizer.decode(prompt['X'].item(), skip_special_tokens=True)}+{tokenizer.decode(prompt['Y'].item(), skip_special_tokens=True)}+{tokenizer.decode(prompt['Z'].item(), skip_special_tokens=True)}="
+    # prompt = f"{tokenizer.decode(prompt['X'].item(), skip_special_tokens=True)}+{tokenizer.decode(prompt['Y'].item(), skip_special_tokens=True)}+{tokenizer.decode(prompt['Z'].item(), skip_special_tokens=True)}="
+    prompt = f"{prompt['X']}+{prompt['Y']}+{prompt['Z']}=" # prompt for numerical causal model
     return tokenizer.encode(prompt, return_tensors='pt')
 
 def main():
